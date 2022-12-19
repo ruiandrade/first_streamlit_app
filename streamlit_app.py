cc72c36906d3be14f5297c9c8485c9264d45c29d
ruiandrade <rvprandrade@gmail.com>
2022-12-19 22:01:08 +0000

    Update streamlit_app.py
    Added New content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streammlit.text('Omega 3 & Blueberry Oatmeal')
+streammlit.text('Kale, Spinach & Rocket Smoothie')
+streammlit.text('Hard-Boiled Free-Range Egg')
